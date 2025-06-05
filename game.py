4eeb0c329adf02d94c99fa4f726ad2b9e2f47a34
Tomasz Nazar <tomeknazz@gmail.com>
2025-06-05 20:42:42 +0200

    Refactor image scaling for pixel art; add method to scale images without blurring

--- game.py
+++ game.py
@@ -148,9 +148,8 @@ class Game:
         # Atrybut z czasem rozpoczęcia gry to późniejszego liczenia go i wyświetlania
         self.start_time = 0
         # Załadowanie tła menu i przeskalowanie go
-
-        self.background_menu = Button._scale_image_pixel_art(pygame.image.load("data/images/menu_bg2.jpg").convert(),
-                                                             RES_WIDTH, RES_HEIGHT)
+        self.background_menu_original = pygame.image.load("data/images/menu_bg2.jpg").convert()
+        self.background_menu = self._scale_pixel_art_image(self.background_menu_original, RES_WIDTH, RES_HEIGHT)
         # Atrybut z obecnym poziomem
         self.current_level = None
         # Add audio instance
@@ -626,6 +625,31 @@ class Game:
                     self.screen.blit(time_text, (10, 10))
                 pygame.display.update()
                 self.clock.tick(60)  # ograniczenie do 60fps
+
+    # Metoda do skalowania obrazu w stylu pixel art bez rozmycia
+    def _scale_pixel_art_image(self, image, target_width, target_height):
+        # Wyłącz wygładzanie
+        pygame.display.set_mode((RES_WIDTH, RES_HEIGHT), pygame.HWSURFACE)
+        pygame.transform.set_smoothscale_backend('GENERIC')
+
+        # Pobierz oryginalne wymiary
+        original_width, original_height = image.get_size()
+
+        # Znajdź największy możliwy mnożnik całkowity
+        scale_x = target_width // original_width
+        scale_y = target_height // original_height
+        scale = max(1, min(scale_x, scale_y))
+
+        # Najpierw skaluj przez mnożnik całkowity (zachowuje ostre piksele)
+        if scale > 1:
+            intermediate_size = (original_width * scale, original_height * scale)
+            intermediate = pygame.transform.scale(image, intermediate_size)
+        else:
+            intermediate = image
+
+        # Następnie skaluj do dokładnego rozmiaru
+        final_image = pygame.transform.scale(intermediate, (target_width, target_height))
+        return final_image
 
 
 if __name__ == "__main__":
